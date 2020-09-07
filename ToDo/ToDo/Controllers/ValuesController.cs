da60b97037377ea54708fe71f9e46fc097327353
markr <mark.rakochyi@gmail.com>
2020-09-07 20:37:11 +0300

    first commit

---
+++ ToDo/ToDo/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ToDo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        DataBase db = new DataBase();
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            db.Add("kek");
+            return db;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ToDo/ToDo/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDo
+{
+    public class DataBase : List<String>
+    {
+
+    }
+}
